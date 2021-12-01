f50546a820ffe088e1269ab5094a9ce8385dbfd8
mustafahegy <mustafa.hegy94@gmail.com>
2021-12-01 13:51:15 +0200

    Initial Commit

---
+++ Swift-Ui-MapKit/Swift_Ui_MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Ui_MapKitApp.swift
+//  Swift-Ui-MapKit
+//
+//  Created by Mostafa on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Ui_MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
